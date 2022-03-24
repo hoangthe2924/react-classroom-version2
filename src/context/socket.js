ff60dfad08f30f0b429f0b368f9e3dff2236a818
Hoang-The Do <73113289+hoangthe2924@users.noreply.github.com>
2022-03-24 15:43:35 +0700

    Update socket.js

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,6 +1,6 @@
 import socketClient from "socket.io-client";
 import * as React from "react";
 
-const SERVER = "http://localhost:7000/";
+const SERVER = "https://classrum-api-v2.herokuapp.com/";
 export const socket = socketClient(SERVER);
 export const SocketContext = React.createContext();
